90d50dbf058bcf4817db661e90c006fd54de6c90
Your Name <you@example.com>
2024-10-09 12:15:03 +0530

    vpc

--- 10-vpc/vpc.tf
+++ 10-vpc/vpc.tf
@@ -1,6 +1,6 @@
 module "vpc" {
     ##source = ../../terraform-aws-security-group
-    source = "git::https://github.com/Raidi13/expense-terraform-dev.git?ref=main"
+    source = "https://github.com/Raidi13/terraform-aws-vpc.git?ref=main"
     vpc_cidr = var.vpc_cidr
     project_name = var.project_name
     enivronment  = var.enivronment
