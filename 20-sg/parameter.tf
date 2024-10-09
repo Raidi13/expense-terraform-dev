3086c7442b483777e85c6fe1995e14a17eba7922
Your Name <you@example.com>
2024-10-09 18:11:09 +0530

    vpc

--- 20-sg/parameter.tf
+++ 20-sg/parameter.tf
@@ -1,28 +1,28 @@
-resource "aws_ssm_parameter" "mysql_sg_id" {
+resource "aws_ssm_Parameter" "mysql_sg_id" {
   ## /expense/dev/mysql_sg_id
   name  = "/${var.project_name}/${var.enivronment}/mysql_sg_id"
   type  = "String"
   value = module.mysql_sg.id
 }
-resource "aws_ssm_parameter" "backend_sg_id" {
+resource "aws_ssm_Parameter" "backend_sg_id" {
   ## /expense/dev/mysql_sg_id
   name  = "/${var.project_name}/${var.enivronment}/backend_sg_id"
   type  = "String"
   value = module.backend_sg.id
 }
-resource "aws_ssm_parameter" "frontend_sg_id" {
+resource "aws_ssm_Parameter" "frontend_sg_id" {
   ## /expense/dev/mysql_sg_id
   name  = "/${var.project_name}/${var.enivronment}/frontend_sg_id"
   type  = "String"
   value = module.frontend_sg.id
 }
-resource "aws_ssm_parameter" "bastion_sg_id" {
+resource "aws_ssm_Parameter" "bastion_sg_id" {
   ## /expense/dev/mysql_sg_id
   name  = "/${var.project_name}/${var.enivronment}/bastion_sg_id"
   type  = "String"
   value = module.bastion_sg.id
 }
-resource "aws_ssm_parameter" "ansible_sg_id" {
+resource "aws_ssm_Parameter" "ansible_sg_id" {
   ## /expense/dev/mysql_sg_id
   name  = "/${var.project_name}/${var.enivronment}/ansible_sg_id"
   type  = "String"
